e9e4726e60bf1d5234a059b7dcb6075f8a32cb2a
unknown <rahulparande04@gmail.com>
2023-08-12 13:35:47 -0400

    Scraper upload and executable

---
+++ run_scraper.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Aug 10 14:24:49 2023
+
+@author: Rahul Parande
+"""
+
+import glassdoor_scrap as gs
+import pandas as pd
+
+path = "C:/Users/Rahul Parande/Documents/ds_salary_proj/chromedriver"
+
+df = gs.get_jobs('data scientist', 10, False, path, 15)
+df
