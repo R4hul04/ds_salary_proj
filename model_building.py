c3670dd5201778ef1903656bb1b6a2b0dbfee342
unknown <rahulparande04@gmail.com>
2023-08-17 20:07:09 -0400

    created fask api endpoint

--- model_building.py
+++ model_building.py
@@ -52,8 +52,8 @@ alpha = []
 error = []
 
 for i in range(1,100):
-    alpha.append(i/100)
-    lml = Lasso(alpha=(i/100))
+    alpha.append(i/1000)
+    lml = Lasso(alpha=(i/1000))
     error.append(np.mean(cross_val_score(lml,X_train,y_train, scoring = 'neg_mean_absolute_error', cv= 3)))
 
 plt.plot(alpha,error)
@@ -61,3 +61,44 @@ plt.plot(alpha,error)
 err = tuple(zip(alpha,error))
 df_err = pd.DataFrame(err, columns = ['alpha','error'])
 df_err[df_err.error == max(df_err.error)]
+
+# random forest
+from sklearn.ensemble import RandomForestRegressor
+rf = RandomForestRegressor()
+
+np.mean(cross_val_score(rf,X_train.values,y_train,scoring = 'neg_mean_absolute_error', cv= 3))
+
+# tune models GridsearchCV
+from sklearn.model_selection import GridSearchCV
+parameters = {'n_estimators':range(10,300,10), 'criterion':('mse','mae'), 'max_features':('auto','sqrt','log2')}
+
+gs = GridSearchCV(rf,parameters,scoring='neg_mean_absolute_error',cv=3)
+gs.fit(X_train.values,y_train)
+
+gs.best_score_
+gs.best_estimator_
+
+# test ensembles
+tpred_lm = lm.predict(X_test)
+tpred_lml = lm_l.predict(X_test)
+tpred_rf = gs.best_estimator_.predict(X_test)
+
+from sklearn.metrics import mean_absolute_error
+mean_absolute_error(y_test,tpred_lm)
+mean_absolute_error(y_test,tpred_lml)
+mean_absolute_error(y_test,tpred_rf)
+
+mean_absolute_error(y_test,(tpred_lm+tpred_rf)/2)
+
+import pickle
+pickl = {'model': gs.best_estimator_}
+pickle.dump( pickl, open( 'model_file' + ".p", "wb" ) )
+
+file_name = "model_file.p"
+with open(file_name, 'rb') as pickled:
+    data = pickle.load(pickled)
+    model = data['model']
+
+model.predict(np.array(list(X_test.iloc[1,:].values)).reshape(1,-1))[0]
+
+list(X_test.iloc[1,:])
